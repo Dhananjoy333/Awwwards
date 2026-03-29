461cc7ed89722db093ec8e2c27199a299d61237a
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-03-30 01:42:42 +0530

    hero section ui and animation done

---
+++ app/_components/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+type Props={
+    id : string;
+    title: string;
+    leftIcon?: React.ReactElement;
+    rightIcon?: React.ReactElement;
+    containerClass: string
+}
+
+function Button({id,title,leftIcon,rightIcon,containerClass}:Props) {
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+        {leftIcon}
+        <span className='relative inline-flex overflow-hidden font-general text-xs uppercase'>
+            <div>
+                {title}
+            </div>
+        </span>
+        {rightIcon}
+    </button>
+  )
+}
+
+export default Button
