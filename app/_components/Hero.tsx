452e40199c4f0992dad12a00ff943dc0cf0c57a7
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-03-30 11:39:22 +0530

    about section scroll image expand implemented

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import Button from "./Button"
 import { TiLocationArrow } from "react-icons/ti"
 import { useGSAP } from "@gsap/react"
